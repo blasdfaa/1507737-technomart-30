bd7e0ee1058a7a19f91a133f73c304654891d833
viktorAZ <72477415+v.zdvacew@gmail.com>
2021-03-14 16:35:35 +0300

    Добавил перелистывание слайдов

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
-
-
 if (document.querySelector('body').classList.contains('page')) {
+  // Slider
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  function nextSlide() {
+    showSlides(slideIndex += 1);
+  }
+
+  function previousSlide() {
+    showSlides(slideIndex -= 1);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let slides = document.getElementsByClassName('promo-slider__item');
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+
+    for (let slide of slides) {
+      slide.style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'flex';
+  }
+
   // Services tabs
 
   const tabsBtn = document.querySelectorAll('.tab-panel__btn');
